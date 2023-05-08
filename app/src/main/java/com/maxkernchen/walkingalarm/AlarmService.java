399eb3ffad62602d6e42fa97383843e40f36d763
maxkernchen <max.f.kernchen@gmail.com>
2023-05-08 16:48:13 -0400

    Removed all left over TODOs

--- app/src/main/java/com/maxkernchen/walkingalarm/AlarmService.java
+++ app/src/main/java/com/maxkernchen/walkingalarm/AlarmService.java
@@ -129,7 +129,6 @@ public class AlarmService extends Service {
      */
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
